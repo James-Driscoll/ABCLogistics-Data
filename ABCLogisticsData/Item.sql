dc71e16c47ed4b436b7e22f23d258ae985d894e6
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-13 17:50:52 +0000

    Added Item table.

---
+++ ABCLogisticsData/Item.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Item] (
+    [PK_ItemID]        INT           IDENTITY (1, 1) NOT NULL,
+    [Weight]           INT           NULL,
+    [SizeCategory]     NCHAR (10)    NULL,
+    [Type]             NVARCHAR (50) NULL,
+    [InsuranceType]    NCHAR (10)    NULL,
+    [RecordedDelivery] NCHAR (10)    NULL,
+    PRIMARY KEY CLUSTERED ([PK_ItemID] ASC)
+);
