9c230ad2a0a5cdfac65fc623aa26b0936637ecd5
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-13 17:51:39 +0000

    Added User table.

---
+++ ABCLogisticsData/User.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[User] (
+    [PK_UserID]            INT            IDENTITY (1, 1) NOT NULL,
+    [FK_BillingAddressID]  INT            NULL,
+    [FK_DeliveryAddressID] INT            NULL,
+    [FirstName]            NVARCHAR (MAX) NULL,
+    [Lastname]             NVARCHAR (MAX) NULL,
+    [MobileNumber]         NVARCHAR (50)  NULL,
+    [LandlineNumber]       NVARCHAR (50)  NULL,
+    [EmailAddress]         NVARCHAR (MAX) NULL,
+    [SystemRole]           NVARCHAR (50)  NULL,
+    [Password]             NVARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([PK_UserID] ASC)
+);
