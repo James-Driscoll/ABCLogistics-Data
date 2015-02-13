1ccefcf123a094c53501d4dade78bd059a8b1ca8
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-13 17:50:00 +0000

    Added Address table.

---
+++ ABCLogisticsData/Address.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Address] (
+    [PK_AddressID] INT            IDENTITY (1, 1) NOT NULL,
+    [Street]       NVARCHAR (MAX) NOT NULL,
+    [Town]         NVARCHAR (MAX) NULL,
+    [County]       NVARCHAR (MAX) NULL,
+    [Postcode]     NVARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([PK_AddressID] ASC)
+);
