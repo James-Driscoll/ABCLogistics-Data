2374c93c76c4a3a0a35dafdf9b2ed878c322cdfb
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-13 17:54:57 +0000

    Added Tracking table.

---
+++ ABCLogisticsData/Tracking.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tracking] (
+    [PK_TrackingID] INT            IDENTITY (1, 1) NOT NULL,
+    [FK_OrderID]    INT            NOT NULL,
+    [Location]      NVARCHAR (MAX) NOT NULL,
+    [Date]          DATE           NOT NULL,
+    [Status]        NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([PK_TrackingID] ASC)
+);
