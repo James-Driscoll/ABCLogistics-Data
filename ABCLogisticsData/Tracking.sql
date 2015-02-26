d113281b67d1f9bc5184dd875926fcbae7bc65b8
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-26 20:06:55 +0000

    Renamed Order field.

--- ABCLogisticsData/Tracking.sql
+++ ABCLogisticsData/Tracking.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Tracking] (
     [Id] INT            IDENTITY (1, 1) NOT NULL,
-    [FK_OrderID]    INT            NOT NULL,
+    [Order]    INT            NOT NULL,
     [Location]      NVARCHAR (50) NOT NULL,
     [Date]          DATE           NOT NULL,
     [Status]        NVARCHAR (50) NOT NULL,
