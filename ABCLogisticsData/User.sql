c0873fec9571a3cbd43b07bfd13a728f8ebd5cbd
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-26 20:34:47 +0000

    Added SystemID column to User table.

--- ABCLogisticsData/User.sql
+++ ABCLogisticsData/User.sql
@@ -3,5 +3,6 @@
     [Name]				   NVARCHAR (50) NULL,
     [ContactNumber]         NVARCHAR (50)  NULL,
     [EmailAddress]         NVARCHAR (50) NULL,
+    [SystemID] NVARCHAR(50) NOT NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
