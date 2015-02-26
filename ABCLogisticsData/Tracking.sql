f21c8f8da6e8891d10405be25b51594463b87200
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-26 18:17:32 +0000

    Updated PK column names.

--- ABCLogisticsData/Tracking.sql
+++ ABCLogisticsData/Tracking.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Tracking] (
-    [PK_TrackingID] INT            IDENTITY (1, 1) NOT NULL,
+    [Id] INT            IDENTITY (1, 1) NOT NULL,
     [FK_OrderID]    INT            NOT NULL,
     [Location]      NVARCHAR (50) NOT NULL,
     [Date]          DATE           NOT NULL,
     [Status]        NVARCHAR (50) NOT NULL,
-    PRIMARY KEY CLUSTERED ([PK_TrackingID] ASC)
+    PRIMARY KEY CLUSTERED ([Id] ASC)
 );
--- ABCLogisticsData/User.sql
+++ ABCLogisticsData/User.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[User] (
-    [PK_UserID]            INT IDENTITY (1, 1) NOT NULL,
+    [Id]            INT IDENTITY (1, 1) NOT NULL,
     [Name]				   NVARCHAR (50) NULL,
     [ContactNumber]         NVARCHAR (50)  NULL,
     [EmailAddress]         NVARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([PK_UserID] ASC)
+    PRIMARY KEY CLUSTERED ([Id] ASC)
 );
