f23af01f53a1853975312842e38818c7d49ef134
james-driscoll <james.e.m.driscoll@gmail.com>
2015-03-05 14:11:08 +0000

    Added Branch FK to Parcel table.

--- ABCLogisticsData/Parcel.sql
+++ ABCLogisticsData/Parcel.sql
@@ -4,5 +4,6 @@
 	[Weight] INT NOT NULL,
 	[Insured] BIT NOT NULL,
 	[DateOrdered] DATE NOT NULL,
-	[Customer] NVARCHAR(50) NOT NULL,
+	[Customer] NVARCHAR(50) NOT NULL, 
+    [Branch] INT NOT NULL,
 )
