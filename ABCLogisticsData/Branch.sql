0f6e7cbc49f9c45f4efbe3f2914902bda4b603b8
james-driscoll <james.e.m.driscoll@gmail.com>
2015-03-05 13:19:52 +0000

    Added Branch table

---
+++ ABCLogisticsData/Branch.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Branch]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [AddressLine1] NVARCHAR(50) NOT NULL, 
+    [AddressLine2] NVARCHAR(50) NOT NULL, 
+    [Town] NVARCHAR(50) NOT NULL, 
+    [County] NVARCHAR(50) NOT NULL, 
+    [Postcode] NVARCHAR(50) NOT NULL, 
+)
