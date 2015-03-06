ee8e934606425a281341109a18ba2a4ef7e182c7
james-driscoll <james.e.m.driscoll@gmail.com>
2015-03-06 14:18:55 +0000

    Removed unused User table.

--- ABCLogisticsData/User.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[User] (
-    [Id]            INT IDENTITY (1, 1) NOT NULL,
-    [Name]				   NVARCHAR (50) NULL,
-    [ContactNumber]         NVARCHAR (50)  NULL,
-    [EmailAddress]         NVARCHAR (50) NULL,
-    [SystemID] NVARCHAR(50) NOT NULL, 
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
